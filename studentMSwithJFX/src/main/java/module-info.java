4a588f4d872008e33172ac07b689240e97d96013
Atman Boulaajaili <60273155+Elon-Fask@users.noreply.github.com>
2022-06-14 18:08:12 +0100

    Delete studentMSwithJFX directory

--- studentMSwithJFX/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.brief7.studentMSwithJFX {
-    requires javafx.controls;
-    requires javafx.fxml;
-	requires java.sql;
-	
-    opens org.brief7.studentMSwithJFX to javafx.fxml;
-    exports org.brief7.studentMSwithJFX;
-}
